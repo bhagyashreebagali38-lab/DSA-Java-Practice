9bd8509d771164839d87bf1c2053e9a8044f2949
Bhagyashree Bagali <bhagyashreebagali38@gmail.com>
2026-03-24 20:47:09 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,9 @@
+class String{
+    public static void main(String args[]){
+        StringBuilder sb=new StringBuilder("Tony");
+        System.out.println(sb);
+        sb.setCharAt(0,'p');
+        System.out.println(sb);
+
+    }
+}
